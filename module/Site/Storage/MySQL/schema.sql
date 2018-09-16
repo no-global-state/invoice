356472d65c800782286e8ffa705953d86b8db106
dave-yang <daworld.ny@gmail.com>
2018-09-16 15:19:00 +0500

    Added basic client attributes

--- module/Site/Storage/MySQL/schema.sql
+++ module/Site/Storage/MySQL/schema.sql
@@ -5,5 +5,8 @@ CREATE TABLE invoices (
     `status` SMALLINT COMMENT 'Invoice status',
     `amount` FLOAT COMMENT 'Invoice amount',
     `token` varchar(32) COMMENT 'MD5-signature',
-    `datetime` DATETIME COMMENT 'Date and time'
+    `datetime` DATETIME COMMENT 'Date and time',
+    `email` varchar(255) COMMENT 'Client email',
+    `phone` varchar(255) COMMENT 'Client phone number',
+    `client` varchar(255) COMMENT 'Client full name'
 );
